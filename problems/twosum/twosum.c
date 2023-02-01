0a7a4eec3853faa7cc4c45e9c9bc56093306a344
Yasotha Ramamoorthy <94780018+YasothaRamamoorthy@users.noreply.github.com>
2023-02-01 19:56:56 +0530

    Update twosum.c

--- problems/twosum/twosum.c
+++ problems/twosum/twosum.c
@@ -1,13 +1,24 @@
-//
-//
-//
-
-/******************************************************
- * 
- *
- *
- ******************************************************/
-
- int *twosum(){
-    
- }
+class Solution {
+public:
+stack <int> p;
+    vector<int> twoSum(vector<int>& nums, int target) {
+        int i=0, j=nums.size()-1;
+    vector<pair<int, int>>v;
+    for(int i=0; i<nums.size(); i++){
+        v.push_back({nums[i], i});
+    }
+    sort(v.begin(), v.end());
+    while(i<j){
+        if(v[i].first+v[j].first==target){
+            break;
+        }
+        else if(v[i].first+v[j].first<target){
+            i++;
+        }
+        else if(v[i].first+v[j].first>target){
+            j--;
+        }
+    }
+    return {v[i].second, v[j].second};
+    }
+};
