751b2a11fb34e01618b90a760bb0cc6122139345
Yasotha Ramamoorthy <94780018+YasothaRamamoorthy@users.noreply.github.com>
2023-02-18 07:13:52 +0530

    Create Invert Binary Tree.cpp

---
+++ problems/twosum/Invert Binary Tree.cpp
@@ -0,0 +1,31 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* invertTree(TreeNode* root) {
+
+    if(root!=NULL)
+    {
+        TreeNode* templeft= root->left;
+        TreeNode*tempright=root->right;
+        root->left=tempright;
+         root->right=templeft;
+    invertTree(root->left);
+    invertTree(root->right);
+    }
+    else 
+    {
+        return NULL;
+    }
+    return root;
+    }
+};
