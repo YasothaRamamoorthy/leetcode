711a16a832033c7cb18ebc8d3ea4fdf6818eba4a
Yasotha Ramamoorthy <94780018+YasothaRamamoorthy@users.noreply.github.com>
2023-02-22 22:38:47 +0530

    Update Fruit Into Baskets.cpp

--- problems/twosum/Fruit Into Baskets.cpp
+++ problems/twosum/Fruit Into Baskets.cpp
@@ -18,7 +18,7 @@ int max(int a,int b)
                     basket.insert(fruits[i]);
                 }
 
-                if(basket.size() < 3)
+                if(basket.size() < 3)//the unique fruits must be 2 not more than 2 bcz we have only two basket to handle two type of fruits
                 {
                 max_size = max(max_size,end-start+1);
                 }
@@ -27,3 +27,59 @@ int max(int a,int b)
         return max_size;
     }
 };
+
+//the above code time complexity is o(n^3) 
+
+class Solution {
+public:
+int max(int a,int b)
+{
+    return a>b?a:b;
+}
+    int totalFruit(vector<int>& fruits) {
+        int max_size=0;
+        for(int start=0;start<fruits.size();start++)
+        {
+            int end=start;
+            set<int> basket;
+            while(end<fruits.size())
+            {
+                if(basket.size() ==2 && basket.find(fruits[end])==basket.end())
+                break;
+
+                basket.insert(fruits[end]);
+                end++;
+            }
+            max_size=max(max_size,end-start);
+        }
+       return max_size; 
+    }
+};
+
+//the above code time complexity is o(n^2)
+
+
+
+
+class Solution {
+public:
+    int totalFruit(vector<int>& fruits) {
+       unordered_map<int,int> basket;
+       int start=0,end=0;
+       for(start,end;end<fruits.size();end++)
+       {
+        basket[fruits[end]]++; 
+       if(basket.size() > 2)
+       {
+        basket[fruits[start]]--;
+       if(basket[fruits[start]]==0)
+         basket.erase(fruits[start]);
+         start++;
+       }
+       }
+       return end - start;
+    }
+};
+
+
+// the above program time complexity is o(n) 
